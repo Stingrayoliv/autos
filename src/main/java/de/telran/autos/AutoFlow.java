4ce6deb53d5ceeea6c984336526cd2eb0ddb8b50
Stingrayoliv <64040033+Stingrayoliv@users.noreply.github.com>
2021-01-10 23:26:51 +0100

    creating Beans,  @Component, @Service, @Repository, @Order(), @Qualifier("simpleAutos")

---
+++ src/main/java/de/telran/autos/AutoFlow.java
@@ -0,0 +1,42 @@
+package de.telran.autos;
+
+import de.telran.autos.entity.Auto;
+import de.telran.autos.service.AutoService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class AutoFlow implements ApplicationRunner {
+
+    final List<Auto> autos;
+    final AutoService autoService;
+    final Auto bentley;
+
+    public AutoFlow(@Qualifier("simpleAutos") List<Auto> autos,
+                    AutoService autoService,
+                    //the name of the argument is important;
+                    Auto silverBentley) {
+        this.autos = autos;
+        this.autoService = autoService;
+        bentley=silverBentley;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        autoService.printAutos(autos);
+
+        for (Auto auto : autos) {
+            if (auto.getMake().equals("Opel")) {
+                autoService.repaintAuto(auto, "grey");
+            }
+        }
+
+        System.out.println("After repaint Opels:");
+        autoService.printAutos(autos);
+        autoService.printAuto(bentley);
+    }
+}
